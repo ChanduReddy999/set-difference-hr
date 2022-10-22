09423d9a6c9b17e604db5118a0d244379b0f3216
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-22 23:06:30 +0530

    Add files via upload

---
+++ set_difference_hr.py
@@ -0,0 +1,11 @@
+"""Set Difference operation in Python (HackerRank Problem)"""
+
+# n=int(input())
+# l=list(input().split())
+# m=int(input())
+# k=list(input().split())
+
+# s1=set(l)
+# s2=set(k)
+
+# print(len(s1.difference(s2)))
